975cac4f25866c3b0d1c413abd13783bec09d9ee
beatsnmelody <beatsnmelody1@gmail.com>
2023-11-28 16:02:28 -0600

    hello world from main index.js

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
-const express = require('express');
+import express from 'express';
+import {fileURLToPath} from 'url';
+// import path from 'path';
+
+const __dirname = fileURLToPath(import.meta.url);
+
 const server = express();
-const port = 4000;
+const port = process.env.port || 5000;
 
 // enable cross-origin resource sharing to proxy api requests
 // from localhost:3000 to localhost:4000 in local dev env
-const cors = require('cors');
+import cors from 'cors';
 server.use(cors());
 
 // create logs for everything
-const morgan = require('morgan');
+import morgan from 'morgan';
 server.use(morgan('dev'));
 
 // handle application/json requests
 server.use(express.json());
 
-// here's our static files
-const path = require('path');
-server.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-  })
-  
-  app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
-
 // here's our API
 // server.use('/api', require('./api'));
-
-// by default serve up the react app if we don't recognize the route
-server.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 // bring in the DB connection
 // const { client } = require('./db');
 
-// define a server handle to close open tcp connection after unit tests have run
-
-// export server and handle for routes/*.test.js
-module.exports = { server, handle };
+server.get('/', (req, res) => {
+  // res.sendFile(__dirname + '/index.html');
+  res.send('Hello World!');
+  })
+  
+  server.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
